abf03854329875eab22bcf80ccb6b309346ddbfa
hzetterstrom <hansz_2000@hotmail.com>
2016-09-27 20:32:47 -0400

    .

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,6 +1,75 @@
 'use strict'
+const getCurrentWeather = require('./lib/getCurrentWeather')
+
+const firstOfEntityRole = function(message, entity, role) {
+  role = role || 'generic';
+
+  const slots = message.slots
+  const entityValues = message.slots[entity]
+  const valsForRole = entityValues ? entityValues.values_by_role[role] : null
+
+  return valsForRole ? valsForRole[0] : null
+}
 
 exports.handle = function handle(client) {
+	
+	const collectCity = client.createStep({
+  satisfied() {
+    return Boolean(client.getConversationState().weatherCity)
+  },
+
+  extractInfo() {
+    const city = firstOfEntityRole(client.getMessagePart(), 'city')
+
+    if (city) {
+      client.updateConversationState({
+        weatherCity: city,
+      })
+	//console log
+      console.log('User wants the weather in:', city.value)
+    }
+  },
+
+  prompt() {
+    client.addResponse('app:response:name:prompt/weather_city')
+    client.done()
+  },
+})
+
+const provideWeather = client.createStep({
+  satisfied() {
+    return false
+  },
+
+  prompt(callback) {
+    getCurrentWeather(client.getConversationState().weatherCity.value, resultBody => {
+      if (!resultBody || resultBody.cod !== 200) {
+        console.log('Error getting weather.')
+        callback()
+        return
+      }
+
+      const weatherDescription = (
+        resultBody.weather.length > 0 ?
+        resultBody.weather[0].description :
+        null
+      )
+
+      const weatherData = {
+        temperature: resultBody.main.temp,
+        condition: weatherDescription,
+        city: resultBody.name,
+      }
+
+      console.log('sending real weather:', weatherData)
+      client.addResponse('app:response:name:provide_weather/current', weatherData)
+      client.done()
+
+      callback()
+    })
+  }
+})
+
   const sayHello = client.createStep({
     satisfied() {
       return Boolean(client.getConversationState().helloSent)
@@ -28,39 +97,13 @@ exports.handle = function handle(client) {
     }
   })
 
-  const handleGreeting = client.createStep({
-    satisfied() {
-      return false
-    },
-
-    prompt() {
-      client.addTextResponse('Hello world, I mean human')
-      client.done()
-    }
-  })
-
-  const handleGoodbye = client.createStep({
-    satisfied() {
-      return false
-    },
-
-    prompt() {
-      client.addTextResponse('See you later!')
-      client.done()
-    }
-  })
-
   client.runFlow({
-    classifications: {
-      goodbye: 'goodbye',
-      greeting: 'greeting'
-    },
-    streams: {
-      goodbye: handleGoodbye,
-      greeting: handleGreeting,
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained]
-    }
-  })
+  classifications: {},
+  streams: {
+    main: 'getWeather',
+    hi: [sayHello],
+    getWeather: [collectCity, provideWeather],
+  }
+})
+
 }
