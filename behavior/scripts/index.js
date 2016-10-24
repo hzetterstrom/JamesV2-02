3b992f08ff6e516e5808bf333ce10d83eaefe9c6
hzetterstrom <hansz@Hanss-MacBook-Air.local>
2016-10-24 10:44:15 -0400

    .

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const getCurrentWeather = require('./lib/getCurrentWeather')
 
 const firstOfEntityRole = function(message, entity, role) {
@@ -12,73 +13,17 @@ const firstOfEntityRole = function(message, entity, role) {
 }
 
 exports.handle = function handle(client) {
-	
-	const collectCity = client.createStep({
-  satisfied() {
-    return Boolean(client.getConversationState().weatherCity)
-  },
-
-  extractInfo() {
-    const city = firstOfEntityRole(client.getMessagePart(), 'city')
-
-    if (city) {
-      client.updateConversationState({
-        weatherCity: city,
-      })
-	//
-      console.log('User wants the weather in:', city.value)
-    }
-  },
-
-  prompt() {
-    client.addResponse('app:response:name:prompt/weather_city')
-    client.done()
-  },
-})
-
-const provideWeather = client.createStep({
-  satisfied() {
-    return false
-  },
-
-  prompt(callback) {
-    getCurrentWeather(client.getConversationState().weatherCity.value, resultBody => {
-      if (!resultBody || resultBody.cod !== 200) {
-        console.log('Error getting weather.')
-        callback()
-        return
-      }
-
-      const weatherDescription = (
-        resultBody.weather.length > 0 ?
-        resultBody.weather[0].description :
-        null
-      )
-
-      const weatherData = {
-        temperature: resultBody.main.temp,
-        condition: weatherDescription,
-        city: resultBody.name,
-      }
-
-      console.log('sending real weather:', weatherData)
-      client.addResponse('app:response:name:provide_weather/current', weatherData)
-      client.done()
-
-      callback()
-    })
-  }
-})
-
   const sayHello = client.createStep({
     satisfied() {
       return Boolean(client.getConversationState().helloSent)
     },
 
     prompt() {
-      client.addTextResponse('Hello world!')
-      client.addTextResponse('I don\'t know much yet, but if you need some pointers on where to get started you should check out the docs – http://docs.init.ai/?key=c0fb-addc-119f')
-      client.addTextResponse('Otherwise, head over to Teach (up at the top) and start teaching me!')
+      client.addResponse('app:response:name:welcome')
+      client.addResponse('app:response:name:provide/documentation', {
+        documentation_link: 'http://docs.init.ai',
+      })
+      client.addResponse('app:response:name:provide/instructions')
       client.updateConversationState({
         helloSent: true
       })
@@ -92,18 +37,72 @@ const provideWeather = client.createStep({
     },
 
     prompt() {
-      client.addTextResponse('Apologies, but this app needs to go back to school!')
+      client.addResponse('app:response:name:apology/untrained')
       client.done()
     }
   })
 
-  client.runFlow({
-  classifications: {},
-  streams: {
-    main: 'getWeather',
-    hi: [sayHello],
-    getWeather: [collectCity, provideWeather],
-  }
-})
+  const collectCity = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().weatherCity)
+    },
 
+    extractInfo() {
+     const city = firstOfEntityRole(client.getMessagePart(), 'city')
+      if (city) {
+        client.updateConversationState({
+          weatherCity: city,
+        })
+        console.log('User wants the weather in:', city.value)
+      }
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:prompt/weather_city')
+      client.done()
+    },
+  })
+
+  const provideWeather = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt(callback) {
+      getCurrentWeather(client.getConversationState().weatherCity.value, resultBody => {
+        if (!resultBody || resultBody.cod !== 200) {
+          console.log('Error getting weather.')
+          callback()
+          return
+        }
+
+        const weatherDescription = (
+          resultBody.weather.length > 0 ?
+          resultBody.weather[0].description :
+          null
+        )
+
+        const weatherData = {
+          temperature: resultBody.main.temp,
+          condition: weatherDescription,
+          city: resultBody.name,
+        }
+
+        console.log('sending real weather:', weatherData)
+        client.addResponse('app:response:name:provide_weather/current', weatherData)
+        client.done()
+
+        callback()
+      })
+    },
+  })
+
+  client.runFlow({
+    classifications: {},
+    streams: {
+      main: 'getWeather',
+      hi: [sayHello],
+      getWeather: [collectCity, provideWeather],
+    }
+  })
 }
